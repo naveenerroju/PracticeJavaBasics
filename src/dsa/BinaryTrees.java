33d213872e201b5751d034a21f76f29ef9c55dc8
Naveen Kumar <naveenkumarerroju@gmail.com>
2022-04-30 11:12:35 +0530

    Added Stack, Queue, BinaryTrees but there is no proper code in them yet.

---
+++ src/dsa/BinaryTrees.java
@@ -0,0 +1,21 @@
+package dsa;
+
+
+public class BinaryTrees {
+
+    class TreeDS{
+        int data;
+        TreeDS leftSubTree,rightSubTree;
+        TreeDS(int data){
+            this.data = data;
+            this.rightSubTree = this.leftSubTree = null;
+        }
+    }
+    TreeDS root;
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/dsa/Queue.java
@@ -0,0 +1,10 @@
+package dsa;
+
+//QUEUE FOLLOWS 'FIRST IN LAST OUT ORDER'
+
+public class Queue {
+    public static void main(String[] args) {
+        Queue traffic = new Queue();
+
+    }
+}
